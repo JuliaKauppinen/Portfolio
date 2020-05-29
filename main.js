da74d70f84af3ad5df81c403b5d55fd949ed9a86
JuliaKauppinen <61182233+JuliaKauppinen@users.noreply.github.com>
2020-05-29 11:34:25 +0200

    commiten

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementsByClassName("toggleMenu")[0].onclick = function(){
+	document.getElementById("myNav").style.height = "100%";
+}
+
+document.getElementsByClassName("closebtn")[0].onclick = function(){
+	document.getElementById("myNav").style.height = "0%";
+}
